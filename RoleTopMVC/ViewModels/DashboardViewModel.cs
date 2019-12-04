46bcfc1fc31e2f4accf643eed06bad1acb13a6b3
Jurandir Junior <jurandir.henrique1999@gmail.com>
2019-12-04 11:03:47 -0300

    Admin e Login Completo

---
+++ RoleTopMVC/ViewModels/DashboardViewModel.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+using RoleTopMVC.Models;
+
+namespace RoleTopMVC.ViewModels
+{
+    public class DashboardViewModel : BaseViewModel
+    {
+        public List<Pedido> Pedidos {get;set;}
+        public uint PedidosAprovados {get;set;}
+        public uint PedidosReprovados {get;set;}
+        public uint PedidosPendentes {get;set;}
+
+        public DashboardViewModel(){
+            this.Pedidos = new List<Pedido>();
+        }
+    }
+}
